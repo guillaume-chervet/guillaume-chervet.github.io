bb4341877f80e14fffa0cd72fdccb668c27054a2
guillaume chervet <guillaume.chervet@gmail.com>
2020-12-18 18:02:01 +0100

    update

---
+++ precache-manifest.894ec037e877f9df7461fa337e29f267.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae67f722f48b65f89556994f36b3902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e154ac52c952b57b7c",
+    "url": "/static/css/2.3d849f48.chunk.css"
+  },
+  {
+    "revision": "5c50e34c5065f9f1d65b",
+    "url": "/static/css/main.15bf5033.chunk.css"
+  },
+  {
+    "revision": "44e154ac52c952b57b7c",
+    "url": "/static/js/2.46473076.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.46473076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c50e34c5065f9f1d65b",
+    "url": "/static/js/main.bf2a8ca8.chunk.js"
+  },
+  {
+    "revision": "bd62b8e008fdd5f52d10",
+    "url": "/static/js/runtime-main.6e896626.js"
+  },
+  {
+    "revision": "d4ae4eac88e337a3e94f28b9ee5d189c",
+    "url": "/static/media/logo-axa.d4ae4eac.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.981267649b7ae0929ac1dd716d56f8f8.js"
+  "/precache-manifest.894ec037e877f9df7461fa337e29f267.js"
 );
 
 self.addEventListener('message', (event) => {
